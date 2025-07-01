196d25f5985a463a17e611718062724793a72a6b
Binoy <dbinoy@gmail.com>
2025-07-01 13:43:29 -0400

    One time cache registration

--- src/app.py
+++ src/app.py
@@ -1,14 +1,15 @@
 import dash
 import dash_bootstrap_components as dbc
+from src.utils.cache import cache
 from src.components.layout import create_layout
+from src.callbacks import register_all_callbacks
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.title = "User Login Telemetry Dashboard"
 server = app.server
-
+cache.init_app(server)
 app.layout = create_layout()
+register_all_callbacks(app) 
 
 if __name__ == "__main__":
-    from src.callbacks import register_all_callbacks
-    register_all_callbacks(app)    
     app.run(host='0.0.0.0', port=8050, debug=True)
--- src/utils/cache.py
+++ src/utils/cache.py
@@ -1,5 +1,4 @@
 from flask_caching import Cache
 from src.config.cache import cache_config
-from src.app import app
 
-cache = Cache(app.server, config=cache_config)
+cache = Cache(config=cache_config)
