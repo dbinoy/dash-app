47612186321efc6a03b885528d795f3931c3d3fc
Binoy <dbinoy@gmail.com>
2025-07-01 17:57:44 -0400

    Azure Cache

--- src/config/cache.py
+++ src/config/cache.py
@@ -1,5 +1,7 @@
+# redis_url = 'redis://localhost:6379/0'
+redis_url = 'redis://:CzlH6N6DtR7JXijW5pS5iykPI7us3NNffAzCaHHG0eY=:@redis-cache-for-dash.redis.cache.windows.net:6380/0'
 cache_config = {
     'CACHE_TYPE': 'redis',
-    'CACHE_REDIS_URL': 'redis://localhost:6379/0',
+    'CACHE_REDIS_URL': redis_url,
     'CACHE_DEFAULT_TIMEOUT': 86400
 }
