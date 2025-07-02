b0cd1cf70b2cc17ffd6eb85f62ab2f1b2b41ee8b
Binoy <dbinoy@gmail.com>
2025-07-01 23:32:06 -0400

    Change Redis URL

--- src/config/cache.py
+++ src/config/cache.py
@@ -1,5 +1,5 @@
 # redis_url = 'redis://localhost:6379/0'
-redis_url = 'redis://:CzlH6N6DtR7JXijW5pS5iykPI7us3NNffAzCaHHG0eY=:@redis-cache-for-dash.redis.cache.windows.net:6380/0'
+redis_url = 'redis://:5BsBnUQZC6zBza620xOK1wF7CSoX5AyZpAzCaF6bQeM=:@dash-redis-cache.redis.cache.windows.net:6380/0'
 cache_config = {
     'CACHE_TYPE': 'redis',
     'CACHE_REDIS_URL': redis_url,
